af4a54cab37bb897fc8145e9f4eb696a8ddf014f
khadijarahmannupur-ctrl <khadijarahmannupur@gmail.com>
2026-05-02 02:11:40 +0600

    fixed trusted origins

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,23 +1,22 @@
 import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
-import client from "./mongodb";
+
+const client = new MongoClient(process.env.MONGODB_URI);
+const db = client.db('tileCarft');
 
 export const auth = betterAuth({
-    database: mongodbAdapter(client),
-
-    trustedOrigins: [
-        "http://localhost:3000",
-        "https://tilecraft-gallery.vercel.app",
-    ],
-
+    database: mongodbAdapter(db, {
+        // Optional: if you don't provide a client, database transactions won't be enabled.
+        client
+    }),
     emailAndPassword: {
         enabled: true,
     },
-
     socialProviders: {
-        google: {
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        },
+        google: { 
+            clientId: process.env.GOOGLE_CLIENT_ID, 
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET, 
+        }, 
     },
 });
