6b6f400a8b9c87f80d1da5254071dd21e0a55bb1
khadijarahmannupur-ctrl <khadijarahmannupur@gmail.com>
2026-05-03 01:51:17 +0600

    baseurl changed

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-    baseURL: "http://localhost:3000"
+    baseURL: "https://tilecraft-gallery.vercel.app"
 });
 
 export const { signIn, signUp, useSession, } = authClient;
