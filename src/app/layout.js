2dea9b51c45671fb114bde707215a816ec3fcc78
khadijarahmannupur-ctrl <khadijarahmannupur@gmail.com>
2026-05-03 03:18:24 +0600

    animate.css added

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,7 @@ import "./globals.css";
 import Navbar from "@/component/Navbar";
 import Footer from "@/component/Footer";
 import { ToastContainer } from 'react-toastify';
+import "animate.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
--- src/component/Banner.jsx
+++ src/component/Banner.jsx
@@ -1,39 +1,39 @@
 import Image from "next/image";
 import Link from "next/link";
-// import Banner from '/images/banner1.jpg'
+
 
 export default function HeroSection() {
     return (
         <section className="relative overflow-hidden bg-[#F8F4EE]">
-            {/* BACKGROUND BLUR */}
+            
             <div className="absolute left-0 top-0 h-72 w-72 rounded-full bg-[#D4A373]/20 blur-3xl"></div>
 
             <div className="absolute bottom-0 right-0 h-80 w-80 rounded-full bg-[#8B5E3C]/10 blur-3xl"></div>
 
             <div className="mx-auto grid min-h-[90vh] max-w-7xl items-center gap-14 px-6 py-16 lg:grid-cols-2 lg:px-8">
-                {/* LEFT CONTENT */}
+                
                 <div>
-                    {/* SMALL BADGE */}
+                    
                     <div className="mb-6 inline-flex items-center rounded-full border border-[#D4A373] bg-[#fffaf4] px-5 py-2 text-sm font-medium text-[#8B5E3C] shadow-sm">
                         Premium Ceramic & Marble Collection
                     </div>
 
-                    {/* HEADING */}
-                    <h1 className="max-w-2xl text-5xl font-bold leading-tight text-[#2D2D2D] md:text-6xl">
+                    
+                    <h1 className="max-w-2xl text-5xl font-bold leading-tight text-[#2D2D2D] md:text-6xl animate__animated animate__zoomIn">
                         Discover Your
                         <span className="block text-[#8B5E3C]">
                             Perfect Aesthetic
                         </span>
                     </h1>
 
-                    {/* DESCRIPTION */}
+                    
                     <p className="mt-7 max-w-xl text-lg leading-8 text-[#6B5B52]">
                         Explore luxury ceramic, marble, and modern tiles crafted
                         to elevate your interiors with timeless elegance and
                         premium aesthetics.
                     </p>
 
-                    {/* BUTTONS */}
+                    
                     <div className="mt-10 flex flex-col gap-4 sm:flex-row">
                         <Link
                             href="/all-tiles"
@@ -47,7 +47,7 @@ export default function HeroSection() {
                         </button>
                     </div>
 
-                    {/* STATS */}
+                    
                     <div className="mt-14 flex flex-wrap gap-10">
                         <div>
                             <h3 className="text-3xl font-bold text-[#8B5E3C]">
