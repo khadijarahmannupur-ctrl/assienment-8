135521d2a74079479a3963922b3c522a7af2fa1d
khadijarahmannupur-ctrl <khadijarahmannupur@gmail.com>
2026-05-03 01:55:08 +0600

    some error fixed

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-    baseURL: "https://tilecraft-gallery.vercel.app"
+    baseURL: process.env.NEXT_PUBLIC_APP_URL
 });
 
 export const { signIn, signUp, useSession, } = authClient;
